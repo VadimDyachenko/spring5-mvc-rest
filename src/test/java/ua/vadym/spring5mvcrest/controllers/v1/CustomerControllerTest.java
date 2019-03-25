fd85d8ab74730875c233ba5cd7c9f0007bc881c8
Vadym Diachenko <vadim.dyachenko@gmail.com>
2019-03-25 07:12:57 +0200

    Refactored CustomerControllerTest class to use json assertion

--- src/test/java/ua/vadym/spring5mvcrest/controllers/v1/CustomerControllerTest.java
+++ src/test/java/ua/vadym/spring5mvcrest/controllers/v1/CustomerControllerTest.java
@@ -8,17 +8,16 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import testtool.JsonFileReader;
 import ua.vadym.spring5mvcrest.api.v1.model.CustomerDTO;
 import ua.vadym.spring5mvcrest.services.CustomerService;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class CustomerControllerTest {
@@ -39,19 +38,19 @@ public class CustomerControllerTest {
     }
 
     @Test
-    public void getCustomerList() throws Exception {
+    public void getAllCustomers() throws Exception {
         //given
         CustomerDTO first = CustomerDTO.builder().firstname("Iron").lastname("Man").url("http://ironman.com").build();
         CustomerDTO second = CustomerDTO.builder().firstname("Super").lastname("Man").url("http://superman.com").build();
         List<CustomerDTO> customers = Arrays.asList(first, second);
         when(service.getAllCustomers()).thenReturn(customers);
 
+        String expected = new JsonFileReader("json/CategoryControllerTest/getAllCustomers_expected.json").content();
+
         //then-when
         mockMvc.perform(get("/api/v1/customers/")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(jsonPath("$.customers", hasSize(2)));
-
+                .andExpect(content().json(expected));
     }
 }
